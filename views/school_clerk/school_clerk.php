333719117029594fc20a730f5e599449efe9984c
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-11-07 19:00:33 +0530

    FA added

--- views/school_clerk/school_clerk.php
+++ views/school_clerk/school_clerk.php
@@ -13,6 +13,7 @@
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../../public/stylesheets/sb-admin.min.css">
+    <link rel="stylesheet" href="../../public/stylesheets/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
 
