78b69e621da9513a1a1de723aafc7e3531d1798a
heshan <heshan.15@cse.mrt.ac.lk>
2017-11-08 01:54:19 +0530

    principal start page - template

---
+++ views/principal/principal.php
@@ -0,0 +1,157 @@
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: heshan-->
+<!-- * Date: 11/8/17-->
+<!-- * Time: 1:26 AM-->
+<!-- */-->
+<!---->
+<!--<html>-->
+<!--<head>-->
+<!--    <title>--><?php
+//        session_start();
+//        echo $_SESSION['login_user'];
+//        ?>
+<!--    </title>-->
+<!--    <link rel="stylesheet" href="../../public/bootstrap/css/bootstrap.min.css">-->
+<!---->
+<!--<!--    <link rel = "stylesheet"-->-->
+<!--<!--          type = "text/css"-->-->
+<!--<!--    <!--          href = "../stylesheets/myStyle.css" />-->-->
+<!--    -->
+<!--    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
+<!--    <link rel="stylesheet" href="../../public/stylesheets/sb-admin.min.css">-->
+<!--    <link rel="stylesheet" href="../../public/stylesheets/font-awesome/css/font-awesome.min.css">-->
+<!--</head>-->
+<!--<body>-->
+<!---->
+<!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">-->
+<!--    <a class="navbar-brand" href="index.html">School Admissions</a>-->
+<!--    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--    </button>-->
+<!--    <div class="collapse navbar-collapse" id="navbarResponsive">-->
+<!--        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">-->
+<!--                <a class="nav-link" href="school_clerk.php">-->
+<!--                    <span class="nav-link-text">Home</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">-->
+<!--                <a class="nav-link" href="add_new_appicant.php">-->
+<!--                    <span class="nav-link-text">Add new Applicant</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">-->
+<!--                <a class="nav-link" href="add_new_guardian.php">-->
+<!--                    <span class="nav-link-text">Add New Guardian</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">-->
+<!--                <a class="nav-link" href="add_old_student.php">-->
+<!--                    <span class="nav-link-text">Add Old Student</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">-->
+<!--                <a class="nav-link" href="#collapseExamplePages" >-->
+<!--                    <span class="nav-link-text">Example Pages</span>-->
+<!--                </a>-->
+<!--                <ul class="sidenav-second-level collapse" id="collapseExamplePages">-->
+<!--                    <li>-->
+<!--                        <a href="login.html">Login Page</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a href="register.html">Registration Page</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a href="forgot-password.html">Forgot Password Page</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a href="blank.html">Blank Page</a>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">-->
+<!--                <a class="nav-link " href="#collapseMulti" >-->
+<!--                    <span class="nav-link-text">Menu Levels</span>-->
+<!--                </a>-->
+<!--                <ul class="sidenav-second-level collapse" id="collapseMulti">-->
+<!--                    <li>-->
+<!--                        <a href="#">Second Level Item</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a href="#">Second Level Item</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a href="#">Second Level Item</a>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>-->
+<!--                        <ul class="sidenav-third-level collapse" id="collapseMulti2">-->
+<!--                            <li>-->
+<!--                                <a href="#">Third Level Item</a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">Third Level Item</a>-->
+<!--                            </li>-->
+<!--                            <li>-->
+<!--                                <a href="#">Third Level Item</a>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">-->
+<!--                <a class="nav-link" href="#">-->
+<!--                    <span class="nav-link-text">Link</span>-->
+<!--                </a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--        <ul class="navbar-nav sidenav-toggler" style="height: 80px;">-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link text-center" id="sidenavToggler">-->
+<!--                </a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--        <ul class="navbar-nav ml-auto">-->
+<!--            <li class="nav-item">-->
+<!--                <form class="form-inline my-2 my-lg-0 mr-lg-2">-->
+<!--                    <div class="input-group">-->
+<!--                        <input class="form-control" type="text" placeholder="Search for...">-->
+<!--                        <span class="input-group-btn">-->
+<!--                <button class="btn btn-primary" type="button">-->
+<!--                  <i class="fa fa-search"></i>-->
+<!--                </button>-->
+<!--              </span>-->
+<!--                    </div>-->
+<!--                </form>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="../../includes/logout.php">-->
+<!--                    <i class="fa fa-fw fa-sign-out"></i>Logout</a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </div>-->
+<!--</nav>-->
+<!---->
+<!--<div class="content-wrapper">-->
+<!--    <div class="container-fluid">-->
+<!--        <div class="jumbotron">-->
+<!--            <h1 class="display-3">Welcome!</h1>-->
+<!--            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>-->
+<!--            <hr class="my-4">-->
+<!--            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>-->
+<!--            <p class="lead">-->
+<!--                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!--<!-- /.container-fluid-->-->
+<!--<!-- /.content-wrapper-->-->
+<!---->
+<!--<script src="../../Javascript/sb-admin.min.js" ></script>-->
+<!--<script src="../../public/bootstrap/js/bootstrap.min.js"></script>-->
+<!---->
+<!--</body>-->
+<!--</html>-->
