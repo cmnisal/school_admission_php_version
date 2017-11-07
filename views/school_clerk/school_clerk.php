ba257ff64673294d376f54a113145f528795429c
heshan <heshan.15@cse.mrt.ac.lk>
2017-11-07 19:07:00 +0530

    icons removed

--- views/school_clerk/school_clerk.php
+++ views/school_clerk/school_clerk.php
@@ -44,19 +44,16 @@
         <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
                 <a class="nav-link" href="#">
-                    <i class="fa fa-fw fa-dashboard"></i>
                     <span class="nav-link-text">Home</span>
                 </a>
             </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
                 <a class="nav-link" href="add_new_appicant.php">
-                    <i class="fa fa-fw fa-area-chart"></i>
                     <span class="nav-link-text">Add new Applicant</span>
                 </a>
             </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
                 <a class="nav-link" href="tables.html">
-                    <i class="fa fa-fw fa-table"></i>
                     <span class="nav-link-text">Tables</span>
                 </a>
             </li>
